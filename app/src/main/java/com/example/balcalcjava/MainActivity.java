c192636aad2bc14ad810d2bb0e66f5718d05d315
Ehor Hashkoder <gashkoder55@gmail.com>
2022-12-08 09:17:59 +0200

    some add

---
+++ app/src/main/java/com/example/balcalcjava/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.balcalcjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+//    private EditText input_vindspeed;
+//    private EditText input_height;
+//    private Button calculate;
+//    private TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        input_vindspeed = findViewById(R.id.input_vindspeed);
+//        input_height = findViewById(R.id.input_height);
+//        calculate = findViewById(R.id.calculate);
+//
+//        calculate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                result.setText("dsfsdf");
+//            }
+//        });
+    }
+}
